4c4f77ed0d6b4337d21de1686258ac750726e502
kuk <hkfaith@tukorea.ac.kr>
2026-05-13 15:37:40 +0900

    .

--- instruction decoder/instruction_decoder.v
+++ instruction decoder/instruction_decoder.v
@@ -1,6 +1,23 @@
 module instruction_decoder (
-input [30:0] instruction
+input [15:0] instruction,
+input [7:0] status,
+output reg alu_operand
 );
+wire opcode = instruction[15:11];
+wire rd = instruction[10:8];
+wire rs = instruction[7:5];
+wire sop1 = instruction[4:2];
+wire sop2 = instruction[1:0];
+
+case (opcode)
+    00000: begin if (sop2 == 2'b00)
+                 else if (sop2 == 2'b01)
+           end
 
 
+
+
+
+    default: 
+endcase
 endmodule
