4f41ceadc2c4ca5adb50f2f2836e09d42a45f23a
kuk <hkfaith@tukorea.ac.kr>
2026-05-17 17:35:20 +0900

    .

--- program counter/program_counter.v
+++ program counter/program_counter.v
@@ -1,1 +1,38 @@
+module program_counter(
+input wire clock,
+input wire [15:0] pc_in,
+input wire pc_wr_en,
+input wire [1:0] pc_sel,
+input wire t1,
+input wire t3,
+input wire t5,
 
+input wire [10:0] offset,
+
+output wire [15:0] next_pc
+);
+reg [15:0] pc = 16'b0;
+
+always @(posedge clock) begin
+    if(pc_wr_en)begin
+    case (pc_sel)
+        2'b00: if(t1)begin
+                pc <= pc + 2;
+            end
+        2'b01: if(t3)begin
+                pc <= pc + offset;
+            end
+        2'b10: if(t3)begin
+                pc <= offset;
+            end
+        2'b11: if(t5)begin
+                pc <= pc_in;
+            end
+        default: pc <= pc + 2;
+    endcase
+    end
+end
+
+assign next_pc = pc;
+
+endmodule
---
+++ state control/state_control.v
@@ -0,0 +1,35 @@
+module state_control(
+input wire clock,
+input wire reset_b,
+
+output wire t1,
+output wire t2,
+output wire t3,
+output wire t4,
+output wire t5,
+
+output wire pc_wr_en
+);
+
+reg [4:0] rotator = 5'b10000;
+ 
+always @(posedge clock or negedge reset_b) begin
+   
+    if(!reset_b)begin
+        rotator <= 5'b10000;
+    end
+    else begin
+        case (rotator)
+            5'b10000:  rotator <= 5'b01000;
+            5'b01000:  rotator <= 5'b00100;
+            5'b00100:  rotator <= 5'b00010;
+            5'b00010:  rotator <= 5'b00001;
+            5'b00001:  rotator <= 5'b10000;
+            default:   rotator <= 5'b10000; 
+        endcase
+    end
+end
+
+assign {t1, t2, t3, t4, t5} = rotator;
+assign pc_wr_en = t1;
+endmodule
