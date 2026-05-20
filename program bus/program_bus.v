3f10b577110976a34def6a69841b7b3fb9ed6c7e
kuk <hkfaith@tukorea.ac.kr>
2026-05-20 23:22:38 +0900

    5/20-night

--- program bus/program_bus.v
+++ program bus/program_bus.v
@@ -1,26 +1,35 @@
 module program_bus (
-input wire clock,
-input wire reset_b,
-input wire t1,
-input wire t3,
-input wire wr_en,
-input wire [15:0] pc,
-input wire [15:0] program_data,
+    input  wire  clock,
+    input  wire  resetb,
+    input  wire  t1,
+    input  wire  t2,
 
-output reg [15:0] pbus_in_data,
-output wire [15:0] pbus_in_address
+    input  wire [15:0] pc,
+    input  wire [15:0] program_data,
+
+    output reg  prdb_pin,
+    output reg  pcsb_pin,
+
+    output wire [15:0] pbus_in_address,
+    output wire [15:0] pbus_in_data
 );
 
 assign pbus_in_address = pc;
+assign pbus_in_data    = program_data;
 
-always @(posedge clock or negedge reset_b) begin
-    if (!reset_b) begin
-        pbus_in_data <= 16'h0000;
+always @(posedge clock or negedge resetb) begin
+    if (!resetb) begin
+        prdb_pin <= 1'b1;
+        pcsb_pin <= 1'b1;
     end
-    else if (wr_en) begin
-        pbus_in_data <= program_data;
+    else if (t1) begin
+        prdb_pin <= 1'b0;
+        pcsb_pin <= 1'b0;
     end
-    
+    else if (t2) begin
+        prdb_pin <= 1'b1;
+        pcsb_pin <= 1'b1;
+    end
 end
 
 endmodule
