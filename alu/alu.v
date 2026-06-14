cd3e0b40491adcac36a4db6b644cadb94567ac6c
kuk <hkfaith@tukorea.ac.kr>
2026-06-14 10:22:00 +0900

    6/14-morning

--- alu/alu.v
+++ alu/alu.v
@@ -40,12 +40,12 @@ always @(*) begin
     alu_result0 = 16'b0;
     alu_result1 = 16'b0;
 
-    alu_cf = 1'b0;
-    alu_zf = 1'b0;
-    alu_nf = 1'b0;
-    alu_vf = 1'b0;
-    alu_sf = 1'b0;
-    alu_hf = 1'b0;
+    alu_cf  = 1'b0;
+    alu_zf  = 1'b0;
+    alu_nf  = 1'b0;
+    alu_vf  = 1'b0;
+    alu_sf  = 1'b0;
+    alu_hf  = 1'b0;
     alu_gtf = 1'b0;
     alu_ltf = 1'b0;
 
@@ -54,100 +54,132 @@ always @(*) begin
     quotient  = 16'b0;
     divisor   = 16'b0;
     remainder = 17'b0;
-if(t3)begin
+
+if(t3) begin
     case (alu_sel)
     `GRP_DATA_SHIFT: begin
         case (alu_sop)
+
         `DS_INC: begin
             temp        = l_operand + 1;
             alu_result0 = temp[15:0];
             alu_cf = temp[16];
+            alu_hf = (l_operand[3:0] == 4'hF);       
             alu_vf = (~l_operand[15]) & alu_result0[15];
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = 1'b0;
         end
 
         `DS_DEC: begin
             temp        = l_operand - 1;
             alu_result0 = temp[15:0];
-            alu_cf = temp[16];
+            alu_hf = (l_operand[3:0] == 4'h0);       
             alu_vf = l_operand[15] & (~alu_result0[15]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
+            alu_sf = 1'b0;
         end
 
         `DS_NEC: begin
             temp        = (~l_operand) + 1;
             alu_result0 = temp[15:0];
-            alu_cf = temp[16];
-            alu_vf = l_operand[15] & alu_result0[15];
+            alu_nf = alu_result0[15];
+            alu_sf = alu_nf;                        
         end
+
         `DS_NOT: begin
             alu_result0 = ~l_operand;
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = alu_nf;                          
         end
 
         `DS_SHL: begin
-            alu_cf          = l_operand[15];
+            alu_cf      = l_operand[15];
             alu_result0 = l_operand << 1;
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_SHR: begin
-            alu_cf          = l_operand[0];
+            alu_cf      = l_operand[0];
             alu_result0 = l_operand >> 1;
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ASL: begin
-            alu_cf          = l_operand[15];
+            alu_cf      = l_operand[15];
             alu_result0 = l_operand <<< 1;
-            alu_vf          = alu_cf ^ alu_result0[15];
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ASR: begin
-            alu_cf          = l_operand[0];
+            alu_cf      = l_operand[0];
             alu_result0 = $signed(l_operand) >>> 1;
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ROL: begin
-            alu_cf          = l_operand[15];
+            alu_cf      = l_operand[15];
             alu_result0 = {l_operand[14:0], l_operand[15]};
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ROR: begin
-            alu_cf          = l_operand[0];
+            alu_cf      = l_operand[0];
             alu_result0 = {l_operand[0], l_operand[15:1]};
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_SHL_V: begin
             if (shift_amt == 0) begin
                 alu_result0 = l_operand;
             end else begin
-                alu_cf          = l_operand[16 - shift_amt];
+                alu_cf      = l_operand[16 - shift_amt];
                 alu_result0 = l_operand << shift_amt;
             end
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_SHR_V: begin
             if (shift_amt == 0) begin
                 alu_result0 = l_operand;
             end else begin
-                alu_cf          = l_operand[shift_amt - 1];
+                alu_cf      = l_operand[shift_amt - 1];
                 alu_result0 = l_operand >> shift_amt;
             end
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ASL_V: begin
             if (shift_amt == 0) begin
                 alu_result0 = l_operand;
             end else begin
-                alu_cf          = l_operand[16 - shift_amt];
+                alu_cf      = l_operand[16 - shift_amt];
                 alu_result0 = l_operand <<< shift_amt;
-                alu_vf          = alu_cf ^ alu_result0[15];
             end
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ASR_V: begin
             if (shift_amt == 0) begin
                 alu_result0 = l_operand;
             end else begin
-                alu_cf          = l_operand[shift_amt - 1];
+                alu_cf      = l_operand[shift_amt - 1];
                 alu_result0 = $signed(l_operand) >>> shift_amt;
             end
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ROL_V: begin
@@ -155,8 +187,10 @@ if(t3)begin
                 alu_result0 = l_operand;
             end else begin
                 alu_result0 = (l_operand << shift_amt) | (l_operand >> (16 - shift_amt));
-                alu_cf          = l_operand[16 - shift_amt];
+                alu_cf      = l_operand[16 - shift_amt];
             end
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         `DS_ROR_V: begin
@@ -164,8 +198,10 @@ if(t3)begin
                 alu_result0 = l_operand;
             end else begin
                 alu_result0 = (l_operand >> shift_amt) | (l_operand << (16 - shift_amt));
-                alu_cf          = l_operand[shift_amt - 1];
+                alu_cf      = l_operand[shift_amt - 1];
             end
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
         end
 
         default: begin
@@ -175,63 +211,95 @@ if(t3)begin
 
         endcase
     end
-
     `GRP_ARITH: begin
         case (alu_sop)
+
         `AR_ADD: begin
             temp        = l_operand + r_operand;
             alu_result0 = temp[15:0];
             alu_cf = temp[16];
+            alu_hf = (l_operand[3:0] + r_operand[3:0]) > 4'hF;
             alu_vf = (~(l_operand[15] ^ r_operand[15])) & (l_operand[15] ^ alu_result0[15]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = 1'b0;
         end
 
         `AR_ADDC: begin
             temp        = l_operand + r_operand + {16'b0, alu_cf_in};
             alu_result0 = temp[15:0];
             alu_cf = temp[16];
+            alu_hf = (l_operand[3:0] + r_operand[3:0] + alu_cf_in) > 4'hF;
             alu_vf = (~(l_operand[15] ^ r_operand[15])) & (l_operand[15] ^ alu_result0[15]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = 1'b0;
         end
 
         `AR_ADDB: begin
             temp        = {9'b0, l_operand[7:0]} + {9'b0, r_operand[7:0]};
             alu_result0 = {l_operand[15:8], temp[7:0]};
             alu_cf = temp[8];
+            alu_hf = (l_operand[3:0] + r_operand[3:0]) > 4'hF;
             alu_vf = (~(l_operand[7] ^ r_operand[7])) & (l_operand[7] ^ temp[7]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = 1'b0;
         end
 
         `AR_ADDBC: begin
             temp        = {9'b0, l_operand[7:0]} + {9'b0, r_operand[7:0]} + {8'b0, alu_cf_in};
             alu_result0 = {l_operand[15:8], temp[7:0]};
             alu_cf = temp[8];
+            alu_hf = (l_operand[3:0] + r_operand[3:0] + alu_cf_in) > 4'hF;
             alu_vf = (~(l_operand[7] ^ r_operand[7])) & (l_operand[7] ^ temp[7]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = 1'b0;
         end
 
         `AR_SUB: begin
             temp        = l_operand - r_operand;
             alu_result0 = temp[15:0];
             alu_cf = temp[16];
+            alu_hf = (l_operand[3:0] < r_operand[3:0]);
             alu_vf = (l_operand[15] ^ r_operand[15]) & (l_operand[15] ^ alu_result0[15]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_sf = 1'b0;
         end
 
         `AR_SUBC: begin
             temp        = l_operand - r_operand - {16'b0, alu_cf_in};
             alu_result0 = temp[15:0];
-            alu_cf = temp[16];
+            alu_hf = (l_operand[3:0] < r_operand[3:0]);
             alu_vf = (l_operand[15] ^ r_operand[15]) & (l_operand[15] ^ alu_result0[15]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
+            alu_sf = 1'b0;
         end
 
         `AR_SUBB: begin
             temp        = {9'b0, l_operand[7:0]} - {9'b0, r_operand[7:0]};
             alu_result0 = {l_operand[15:8], temp[7:0]};
-            alu_cf = temp[8];
+            alu_hf = (l_operand[3:0] < r_operand[3:0]);
             alu_vf = (l_operand[7] ^ r_operand[7]) & (l_operand[7] ^ temp[7]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
+            alu_sf = 1'b0;
         end
 
         `AR_SUBBC: begin
             temp        = {9'b0, l_operand[7:0]} - {9'b0, r_operand[7:0]} - {8'b0, alu_cf_in};
             alu_result0 = {l_operand[15:8], temp[7:0]};
-            alu_cf = temp[8];
+            alu_hf = (l_operand[3:0] < r_operand[3:0]);
             alu_vf = (l_operand[7] ^ r_operand[7]) & (l_operand[7] ^ temp[7]);
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
+            alu_sf = 1'b0;
         end
 
         `AR_MUL: begin
@@ -242,6 +310,9 @@ if(t3)begin
             end
             alu_result0 = mul_temp[15:0];
             alu_result1 = mul_temp[31:16];
+            alu_nf = alu_result0[15];
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
         end
 
         `AR_MULHB: begin
@@ -251,6 +322,9 @@ if(t3)begin
                     mul_temp = mul_temp + ({24'b0, l_operand[15:8]} << i);
             end
             alu_result0 = mul_temp[15:0];
+            alu_nf = 1'b0;
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = mul_temp[16];
         end
 
         `AR_MULLB: begin
@@ -260,6 +334,9 @@ if(t3)begin
                     mul_temp = mul_temp + ({24'b0, l_operand[7:0]} << i);
             end
             alu_result0 = mul_temp[15:0];
+            alu_nf = 1'b0;
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = mul_temp[16];
         end
 
         `AR_DIV: begin
@@ -282,6 +359,9 @@ if(t3)begin
                 alu_result0 = 16'hFFFF;
                 alu_result1 = 16'hFFFF;
             end
+            alu_nf = 1'b0;
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
         end
 
         `AR_DIVB: begin
@@ -304,6 +384,9 @@ if(t3)begin
                 alu_result0 = 16'hFFFF;
                 alu_result1 = 16'hFFFF;
             end
+            alu_nf = 1'b0;
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
         end
 
         `AR_MOD: begin
@@ -324,6 +407,9 @@ if(t3)begin
             end else begin
                 alu_result0 = 16'hFFFF;
             end
+            alu_nf = 1'b0;
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
         end
 
         `AR_MODB: begin
@@ -344,6 +430,9 @@ if(t3)begin
             end else begin
                 alu_result0 = 16'hFFFF;
             end
+            alu_nf = 1'b0;
+            alu_zf = (alu_result0 == 16'b0);
+            alu_cf = 1'b0;
         end
 
         default: begin
@@ -356,35 +445,34 @@ if(t3)begin
 
     `GRP_LOGIC: begin
         case (alu_sop)
-        // Logical
         `LG_AND: begin
             alu_result0 = l_operand & r_operand;
         end
-
         `LG_OR: begin
             alu_result0 = l_operand | r_operand;
         end
-
         `LG_XOR: begin
             alu_result0 = l_operand ^ r_operand;
         end
-
         `LG_NOR: begin
             alu_result0 = ~(l_operand | r_operand);
         end
-
         default: begin
             alu_result0 = 16'b0;
         end
-
         endcase
+        alu_vf = 1'b0;
+        alu_nf = alu_result0[15];
+        alu_zf = (alu_result0 == 16'b0);
+        alu_sf = alu_nf;              
     end
-
+    
     `GRP_CMP: begin
         temp        = l_operand - r_operand;
         alu_result0 = temp[15:0];
-        alu_gtf = (~alu_result0[15]) & (alu_result0 != 16'b0) & (~alu_vf);
-        alu_ltf = alu_result0[15] ^ alu_vf;
+        alu_zf  = (alu_result0 == 16'b0);
+        alu_gtf = (~alu_result0[15]) & (~alu_zf);
+        alu_ltf = alu_result0[15];
     end
 
     default: begin
@@ -392,13 +480,8 @@ if(t3)begin
         alu_result1 = 16'b0;
     end
 
-    endcase 
-    alu_zf = (alu_result0 == 16'b0);
-    alu_nf = alu_result0[15];
-    alu_sf = alu_nf ^ alu_vf;
-    alu_hf = (l_operand[3:0] + r_operand[3:0]) > 4'hF;
+    endcase
 end
-   
 
 end
 endmodule
